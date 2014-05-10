cb9b2a67be641eec828f22201aa8f661988b2978
Fábio Nave <nave.fabio@gmail.com>
2014-05-10 09:53:20 +0100

    teste dos args

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,8 +2,14 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Ola Mundo 2 :D");
-
+		System.out.print("Ola ");	
+		for(int i=0; i< args.length; i++){
+			if(args.length-1!=i){
+				System.out.print(args[i]+" ");
+			}else{
+				System.out.println(args[i]);
+			}
+		}		
 	}
 
 }
